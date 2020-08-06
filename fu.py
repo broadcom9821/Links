dffdc2cd5a8d1922698d66d5ba651dd8a55684b2
broadcom9821 <44141692+broadcom9821@users.noreply.github.com>
2020-08-06 20:43:53 +0300

    Update fu.py

--- fu.py
+++ fu.py
@@ -1,1 +1,4 @@
 sudo apt-get remove fonts-opensymbol libreoffice libreoffice-\* openoffice.org-dtd-officedocument1.0 python\*-uno uno-libs3-\* ure ure-dbg
+sudo apt-get remove --purge libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
